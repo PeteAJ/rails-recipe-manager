91ed56a513c729b6e48b03beb6e0e6627d0a69ac
Peter Jacobson <github email address>
2016-08-01 13:50:19 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+ # roots
+  root "static_pages#home"
+
+  resources :recipes
+  resources :users
+
 end
