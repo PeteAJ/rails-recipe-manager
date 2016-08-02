2a47340b11a066f04153d580e9225342127e51b3
Peter Jacobson <github email address>
2016-08-02 11:04:56 -0400

    sessions controller/view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  has_secure_password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :recipes
-  
+  has_many :ingredients
 end
